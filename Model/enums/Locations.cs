255337aac8038c0153fe1bb20f394f5b5703d09c
suicide <42965940+622022@users.noreply.github.com>
2020-03-10 10:16:28 +0100

    Enums added for the models

---
+++ Model/enums/Locations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosqlModel.enums
+{
+    public enum Locations
+    {
+        Amsterdam,
+        Haarlem,
+        Knuppeldam,
+        Headquarters
+    }
+}
---
+++ Model/enums/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosqlModel.enums
+{
+    public enum Role
+    {
+        Employee,
+        Admin
+    }
+}
