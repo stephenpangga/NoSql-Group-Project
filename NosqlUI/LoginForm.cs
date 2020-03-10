9e2b7f62307465b7194567e77d28e11948de7fed
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-03-10 10:17:29 +0100

    FORM PRACTICE

---
+++ NosqlUI/LoginForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NosqlUI
+{
+    public partial class LoginForm : BaseForm
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DashBoardForm dashboard = new DashBoardForm();
+
+            dashboard.Show();
+            this.Hide();
+        }
+    }
+}
