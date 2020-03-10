413e36f3f7637429869f0b122cf97fcc408f2630
suicide <42965940+622022@users.noreply.github.com>
2020-03-10 10:30:08 +0100

    Enums added for Model

---
+++ Model/enums/PriorityType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosqlModel.enums
+{
+    public enum PriorityType
+    {
+        Low,
+        Normal,
+        High
+    }
+}
