713566ac67e2ac802c593e539d8da67c3d177b0a
suicide <42965940+622022@users.noreply.github.com>
2020-03-10 10:03:22 +0100

    MVC setup

---
+++ NosqlDAL/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace NosqlDAL
+{
+    class Base
+    {
+        public static void OpenConnection()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://622022:MongoDB454%21@awesomecluster-7pybh.mongodb.net/test?retryWrites=true&w=majority");
+
+        }
+
+        //different methods for queries come here
+    }
+}
---
+++ NosqlLogic/Employees_Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NosqlLogic
+{
+    class Employees_Logic
+
+
+    {
+        //methods for employees
+
+        //public List<Employee> Get_Employees()
+        //{
+        //    string query = 
+        //    return the list
+        //}
+    }
+}
