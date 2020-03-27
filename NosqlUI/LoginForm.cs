e32c0d72be9abf99a9bcf44f555e093091d702be
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-03-27 09:47:30 +0100

    pseudo code for login

--- NosqlUI/LoginForm.cs
+++ NosqlUI/LoginForm.cs
@@ -25,7 +25,24 @@ namespace NosqlUI
         private void button4_Click(object sender, EventArgs e)
         {
             DashBoardForm dashboard = new DashBoardForm();
-
+            /*
+            if (check username)
+            {
+                if (check password)
+                {
+                    //use switch here maybe?
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect Password", "", MessageBoxButtons.OK);
+                }
+            }
+            else
+            {
+                //if user doesnt not exist, show this message
+                MessageBox.Show("the user does not exist.","", MessageBoxButtons.OK);
+            }
+            */
             //send user to different forms(employee or customer) based on their roles
 
             dashboard.Show();
