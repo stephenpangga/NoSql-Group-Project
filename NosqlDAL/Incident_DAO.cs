9bd1a6a61671c9d9f2357286f141d2d2d102e61f
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-04-03 14:10:08 +0200

    fix error when nofal change getspecficdocumentlist method at dao

--- NosqlDAL/Incident_DAO.cs
+++ NosqlDAL/Incident_DAO.cs
@@ -121,7 +121,7 @@ namespace NosqlDAL
         public List<Incident> getSpecificIncidentTickets(string ticketStatus, string column)
         {
             List<Incident> incidents = new List<Incident>();
-            var document = GetSpecificDocumentsList("Incidents", ticketStatus, column);
+            var document = GetSpecificDocumentList("Incidents", ticketStatus, column);
             foreach (var info in document)
             {
                 ObjectId id = (ObjectId)info["_id"];
