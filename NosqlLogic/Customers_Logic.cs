cbe0c048ffdc86345272a9cf4bdbe945619eee82
suicide <42965940+622022@users.noreply.github.com>
2020-04-03 12:37:18 +0200

    Fixed instances of code repetition.

--- NosqlLogic/Customers_Logic.cs
+++ NosqlLogic/Customers_Logic.cs
@@ -26,18 +26,9 @@ namespace NosqlLogic
 
         public void SendEmail(string email)
         {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            var stringChars = new char[8];
-            var random = new Random();
-
-            for (int i = 0; i < stringChars.Length; i++)
-            {
-                stringChars[i] = chars[random.Next(chars.Length)];
-            }
-
-            var finalString = new String(stringChars);
-            customer_DAO.sendEmail(email, finalString);
-            customer_DAO.resetEmail("Users", email, "email", finalString, "password");
+            string pass= getNewPassword();
+            customer_DAO.sendEmail(email, pass);
+            customer_DAO.resetEmail("Users", email, "email", pass, "password");
         }
 
         //Tim - Send the new user info to the database
@@ -68,17 +59,17 @@ namespace NosqlLogic
 
         public string getNewPassword() 
         {
-            string password = "";
-            string passstring = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-            StringBuilder res = new StringBuilder();
-            Random rnd = new Random();
-            for(int i = 0; i < 6; i++)
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
             {
-                int number = rnd.Next(0, 62);
-                string sub = passstring.Substring(number, 1);
-                password = password + sub;
+                stringChars[i] = chars[random.Next(chars.Length)];
             }
-            return password;
+
+            var finalString = new String(stringChars);
+            return finalString;
         }
     }
 }
